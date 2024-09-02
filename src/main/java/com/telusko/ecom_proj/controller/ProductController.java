7fefca8866de8977fe1eb4e160289db5be021a29
Manav Gupta <37526340+manav98@users.noreply.github.com>
2024-09-02 22:36:25 +0530

    Added response message implementation

--- src/main/java/com/telusko/ecom_proj/controller/ProductController.java
+++ src/main/java/com/telusko/ecom_proj/controller/ProductController.java
@@ -3,7 +3,10 @@ package com.telusko.ecom_proj.controller;
 import com.telusko.ecom_proj.model.Product;
 import com.telusko.ecom_proj.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -15,12 +18,29 @@ public class ProductController {
     private ProductService service;
 
     @GetMapping("/products")
-    public List<Product> getAllProducts() {
-        return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
 
     @GetMapping("/product/{id}")
-    public Product getProduct(@PathVariable int id){
-        return service.getProductById(id);
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
+        Product product = service.getProductById(id);
+        if (product != null) {
+
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile) {
+        try {
+            Product product1 = service.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.CREATED);
+        } catch (Exception exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
---
+++ src/main/resources/data2.sql
@@ -0,0 +1,60 @@
+INSERT INTO product (
+        name,
+        desc,
+        brand,
+        price,
+        category,
+        release_date,
+        available,
+        quantity
+    )
+VALUES (
+        'TATA Nexon',
+        'A mini suv car with good safety features',
+        'TATA',
+        1200000,
+        'mini-SUV',
+        '2024-08-30T14:39:14.321+00:00',
+        true,
+        1000
+    ),
+    (
+        'TATA Punch',
+        'A hatchback car with good safety features',
+        'TATA',
+        800000,
+        'Hatchback',
+        '2024-08-30T14:39:14.321+00:00',
+        true,
+        1600
+    ),
+    (
+        'Toyota Fortuner',
+        'A SUV car with good safety and political safety features',
+        'Toyota',
+        5000000,
+        'SUV',
+        '2024-08-30T14:39:14.321+00:00',
+        true,
+        160
+    ),
+    (
+        'Hyundai i10',
+        'A hatchback car with good interior',
+        'Hyundai',
+        750000,
+        'Hatchback',
+        '2024-08-30T14:39:14.321+00:00',
+        true,
+        4000
+    ),
+    (
+        'Suzuki WagonR',
+        'A hatchback car',
+        'Suzuki',
+        600000,
+        'Hatchback',
+        '2024-08-30T14:39:14.321+00:00',
+        true,
+        16000
+    );
